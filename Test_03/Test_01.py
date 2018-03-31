f49228fde5c72c8d780abe08b1501cdd30107b11
lily <lily@foxmail.com>
2018-03-31 16:52:01 +0800

    测试

---
+++ Test_03/Test_01.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+class Test_allure:
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    # @allure.step("我是测试步骤001")
+    def test_al(self):
+        assert 1
+
+    def test_al1(self):
+        assert 1
+
+    def test_al2(self):
+        assert 0
+
+    def test_al3(self):
+        assert 0
+
+    def test_al4(self):
+        assert 1
+
+    # def test_al5(self):
+    #     assert 0
